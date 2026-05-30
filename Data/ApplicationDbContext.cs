70bcc12e5057c940877362230f6617248e6cf74b
fernandoprincipecuellar <fernandoprincipe20.a@gmail.com>
2026-05-29 20:07:49 -0500

    inicial

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace PRACTICA_03.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+{
+}
